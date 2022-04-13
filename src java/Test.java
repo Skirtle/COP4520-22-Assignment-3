9e1ebee4d19c9704351d726a1cff07537f189fab
Dalton <daltonkajander@knights.ucf.edu>
2022-04-12 22:59:46 -0400

    Random Array
    Generate a random array with no duplicates

--- src java/Test.java
+++ src java/Test.java
@@ -1,14 +1,37 @@
+import java.util.Random;
+import java.util.Arrays;
+
 public class Test {
     static final int THREADS = 4;
     static final int PRESENTS = 10; // 5000000
 
     public static void main(String[] args) {
-        ConcurrentLinkedList ll = new ConcurrentLinkedList();
+        SequentialLinkedList ll = new SequentialLinkedList();
 
         for (int i = 0; i < PRESENTS; i++) {
             ll.add(i);
         }
 
         ll.print();
+    }
+
+    static int[] randomArray() {
+        int[] arr = new int[PRESENTS];
+        Random random = new Random();
+
+        for (int i = 0; i < PRESENTS; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = PRESENTS - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        // System.out.println(Arrays.toString(arr));
+
+        return arr;
     }
 }
