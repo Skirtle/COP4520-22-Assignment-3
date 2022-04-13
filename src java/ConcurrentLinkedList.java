8cc66c4abce52475047d9f740f3e8154e7b51ea7
Dalton <daltonkajander@knights.ucf.edu>
2022-04-13 18:38:09 -0400

    Made Node its own file

--- src java/ConcurrentLinkedList.java
+++ src java/ConcurrentLinkedList.java
@@ -94,19 +94,3 @@ public class ConcurrentLinkedList {
 	}
 
 }
-
-class Node {
-	Node next;
-	int data;
-
-	public Node(int data) {
-		this.data = data;
-		this.next = null;
-	}
-
-	public Node(int data, Node next) {
-		this.data = data;
-		this.next = next;
-	}
-
-}
---
+++ src java/Node.java
@@ -0,0 +1,15 @@
+class Node {
+    Node next;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
