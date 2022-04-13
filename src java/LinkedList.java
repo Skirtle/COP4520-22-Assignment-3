9d470c4b853365cf95d2ca711396ac85a670e935
Dalton <daltonkajander@knights.ucf.edu>
2022-04-12 22:08:48 -0400

    Testing Java

---
+++ src java/LinkedList.java
@@ -0,0 +1,11 @@
+public class LinkedList {
+	public int val = 0;
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+
+	public LinkedList() {
+		this.val = 69;
+	}
+}
---
+++ src java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static final int THREADS = 4;
+    static final int PRESENTS = 500000;
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        System.out.println(ll.val);
+    }
+}
