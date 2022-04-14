37e572a6bad770473f076d840327287439c7b480
Dalton <daltonkajander@knights.ucf.edu>
2022-04-13 23:35:22 -0400

    Update Problem2.java

--- Problem 2/Problem2.java
+++ Problem 2/Problem2.java
@@ -1,28 +1,69 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Problem2 {
     static final int THREADS = 8;
     static final int[] TEMP_RANGE = { -100, 70 };
 
     public static void main(String[] args) throws InterruptedException {
-        Sensor[] sensors = new Sensor[THREADS];
-        Thread[] threads = new Thread[THREADS];
+        int hours = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount of hours to report: ");
+        hours = scanner.nextInt();
+        scanner.close();
 
-        for (int i = 0; i < THREADS; i++) {
-            sensors[i] = new Sensor(i);
-            threads[i] = new Thread(sensors[i]);
+        for (int hour = 0; hour < hours; hour++) {
+            Sensor[] sensors = new Sensor[THREADS];
+            Thread[] threads = new Thread[THREADS];
+
+            for (int i = 0; i < THREADS; i++) {
+                sensors[i] = new Sensor(i);
+                threads[i] = new Thread(sensors[i]);
+            }
+
+            for (int i = 0; i < THREADS; i++) {
+                threads[i].start();
+            }
+
+            for (int i = 0; i < THREADS; i++) {
+                threads[i].join();
+            }
+
+            // System.out.println(Arrays.toString(Sensor.temps));
+            int[] sorted = new int[480];
+            for (int i = 0; i < 480; i++) {
+                sorted[i] = Sensor.temps[i];
+            }
+            ArrayList<Integer> fixed = sortUnique(sorted);
+
+            // Report
+            System.out.println("Hour " + (hour + 1));
+            System.out.print("\t5 lowest temperatures: " + fixed.get(0));
+            for (int i = 1; i < 5; i++) {
+                System.out.print(", " + fixed.get(i));
+            }
+            System.out.println();
+            System.out.print("\t5 highest temperatures: " + fixed.get(fixed.size() - 1));
+            for (int i = fixed.size() - 2; i > fixed.size() - 6; i--) {
+                System.out.print(", " + fixed.get(i));
+            }
+            System.out.println();
         }
 
-        for (int i = 0; i < THREADS; i++) {
-            threads[i].start();
+    }
+
+    public static ArrayList<Integer> sortUnique(int[] arr) {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        Arrays.sort(arr);
+        for (int i = 0; i < 480; i++) {
+            if (newList.contains(arr[i]))
+                continue;
+            newList.add(arr[i]);
         }
 
-        for (int i = 0; i < THREADS; i++) {
-            threads[i].join();
-        }
-
-        System.out.println(Arrays.toString(Sensor.temps));
+        return newList;
 
     }
 }
